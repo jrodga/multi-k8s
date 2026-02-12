fc4f64c5a446a8a394964f2b8970769161a649cf
jrodga <jrodga1604@gmail.com>
2026-02-12 11:07:09 +0000

    first commit

--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -3,3 +3,11 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import axios from 'axios';
+
+jest.mock('axios');
+
+beforeEach(() => {
+  axios.get.mockResolvedValue({ data: [] });
+});
+
